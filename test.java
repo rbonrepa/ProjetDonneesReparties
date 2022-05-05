581d440e60c654a53bb195a783f1cc6f95363b93
Arnaud <62288645+ArnaudLiN7@users.noreply.github.com>
2022-05-05 18:15:53 +0200

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] a) {
+
+        int i = 0;
+
+        while(i < 5) {
+            i++;
+            System.out.println("OK");        
+        }
+
+        System.out.println("FIN");
+
+    }
+}
